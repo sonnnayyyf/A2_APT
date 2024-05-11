e9fef168248c6abf0550dc328e0e03b14151e6d6
Nguyen Huu Khoi <119044897+Datduud@users.noreply.github.com>
2024-05-11 10:29:20 +0700

    Add files via upload

--- fileLoader.cpp
+++ fileLoader.cpp
@@ -50,8 +50,6 @@ bool FileLoader::loadCoinData(string coinsFile, Bank *bank)
     {
         cout << "File could not be opened" << endl;
     }
-    // closing file
-    inputFile.close();
     return success;
 }
 
@@ -160,7 +158,12 @@ bool FileLoader::loadFoodData(string foodsFile, LinkedList *list)
 
     // If any of the validation checks fail return false
     // If all checks pass and data is read return true
-    // Closing file
-    inputFile.close();
     return success;
+}
+
+void FileLoader::enterFoodData(string foodsFile, LinkedList *list){
+
+}
+void FileLoader::enterCoinData(string coinsFile, Bank* bank){
+    
 }
--- helper.cpp
+++ helper.cpp
@@ -15,7 +15,7 @@ bool Helper::isNumber(string s)
     }
     catch (const std::invalid_argument &e)
     {
-        isNumber = false;
+        isNumber = true;
     }
     return isNumber;
 }
@@ -35,12 +35,3 @@ void Helper::splitString(string s, vector<string> &tokens, string delimeter)
     }
     delete[] _s;
 }
-
-// string Helper::readInput()
-// {
-//     string input;
-//     std::getline(std::cin, input);
-//     std::cout << std::endl;
-
-//     return input;
-// }
