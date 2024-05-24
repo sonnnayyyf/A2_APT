6936d4637584370224079b0d6ce4331b066f4d1e
Nguyen Huu Khoi <119044897+Datduud@users.noreply.github.com>
2024-05-24 20:00:48 +0700

    Update vendingMachine.h

--- vendingMachine.h
+++ vendingMachine.h
@@ -8,19 +8,19 @@
 class VendingMachine
 {
 public:
-    VendingMachine(LinkedList *foods, Bank *bank, FileLoader loader); // Done
-    ~VendingMachine();
+    VendingMachine(LinkedList *foods, Bank *bank, FileLoader loader); // constructor
+    ~VendingMachine(); // deconstructor
 
-    bool loadVendingMachine(std::string foodFilePath, std::string coinFilePath);
-    void addItem();
-    bool removeItem(); // Done
-    void purchaseItem();
-    void saveData(std::string foodFilePath, std::string coinFilePath); // Done
-    void displayItems();                                               // Done
-    void displayBalance();
-    void cancelPurchase(vector<int> paidBills); // Done
-    void cancelRefund(vector<int> refundedBills);
-    bool refund(unsigned int Amount, int index, std::string &changeOutput, vector<int> refundedBills);
+    bool loadVendingMachine(std::string foodFilePath, std::string coinFilePath); // initialize data files
+    void addItem();// add a new meal
+    bool removeItem(); // remove a meal
+    void purchaseItem(); // purchase a meal
+    void saveData(std::string foodFilePath, std::string coinFilePath); // save the data before exit
+    void displayItems(); // print menu
+    void displayBalance(); // print all coins in system, their amount and total
+    void cancelPurchase(vector<int> paidBills); // return all paid coins to customer
+    void cancelRefund(vector<int> refundedBills); // take back all refunded coins
+    bool refund(unsigned int Amount, int index, std::string &changeOutput, vector<int> refundedBills); // return the change to customer after purchase a meal
 
 private:
     Bank *bank;
