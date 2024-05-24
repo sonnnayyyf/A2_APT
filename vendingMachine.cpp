3ce74249eb5a7b5213ef3a55eb84f8f23c992da6
Nguyen Huu Khoi <119044897+Datduud@users.noreply.github.com>
2024-05-24 18:59:15 +0700

    Comment vendingMachine.cpp

--- vendingMachine.cpp
+++ vendingMachine.cpp
@@ -26,6 +26,7 @@ VendingMachine::~VendingMachine() {
   delete this->foods;
 }
 
+// initialize data files into system
 bool VendingMachine::loadVendingMachine(string foodFilePath,
                                         string coinFilePath) {
   bool success = false;
@@ -36,6 +37,7 @@ bool VendingMachine::loadVendingMachine(string foodFilePath,
   return success;
 }
 
+//add new meal to the system
 void VendingMachine::addItem() {
   bool adding = true;
   string id = "";
@@ -136,6 +138,7 @@ void VendingMachine::addItem() {
   }
 }
 
+// remove a meal from the system
 bool VendingMachine::removeItem() {
   bool success = false;
   bool removing = true;
@@ -171,6 +174,7 @@ bool VendingMachine::removeItem() {
   return success;
 }
 
+// purchase a meal
 void VendingMachine::purchaseItem() {
   cout << "Purchase Meal:\n";
   cout << "-------------\n";
@@ -181,6 +185,7 @@ void VendingMachine::purchaseItem() {
   if (choice.empty() || cin.eof()) {
     cout << endl << "Purchase cancelled!";
   } else {
+    // remove potential '\n' from input line
     Helper::removeWhitespace(choice);
     FoodItem *curr = this->foods->get(choice);
 
@@ -198,7 +203,9 @@ void VendingMachine::purchaseItem() {
       int dollars = curr->price->dollars;
       int cents = curr->price->cents;
 
+      // this vector record all paid coin to return to the customer when the purchase is cancelled
       vector<int> paid_bills = {};
+      // calculate total price in cents
       int toPay = dollars * 100 + cents;
       while (toPay > 0) {
         cout << "You still need to give us $ " + std::to_string(toPay / 100) +
@@ -210,18 +217,21 @@ void VendingMachine::purchaseItem() {
         }
 
         std::getline(cin, choice);
-
+        // check for End of file or Enter on empty line
         if (cin.eof() || choice.empty()) {
           cout << endl << "Purchase cancelled!\n";
           toPay = 0;
+          // return all coins the customer paid, taken from the bank
           cancelPurchase(paid_bills);
         } else {
           choice.erase(choice.find_last_not_of(" \t\r\n\v\f") + 1);
+          // check if the input is a number
           if (Helper::isNumber(choice) == false) {
             cout << "Error: invalid denomination encountered.\n";
           } else {
             int payment = stoi(choice);
             if (bank->manageBalance(payment, ADD, 1)) {
+              // add the paid coin to the save list
               paid_bills.push_back(payment);
               toPay -= payment;
             }
@@ -229,19 +239,25 @@ void VendingMachine::purchaseItem() {
         }
       }
 
+      // reverse toPay to positive for future use
       toPay = 0 - toPay;
+      // refund
       if (toPay > 0) {
         string changeOutput = "Your change is ";
         vector<int> refundedBills = {};
+        // check if possible to refund with coins available in bank and print the result
         if (refund(toPay, 0, changeOutput, refundedBills)) {
           cout << changeOutput << endl;
+          // reduce the available meal count (after refund)
           curr->onHand -= 1;
         } else {
           cout << "Insufficient change in system. Purchase cancelled.\n";
+          // cancel the purchase process when don't have enough coins
           cancelPurchase(paid_bills);
           cancelRefund(refundedBills);
         }
       } else {
+        // reduce the available meal count (no change)
         curr->onHand -= 1;
       }
     } else {
@@ -251,31 +267,38 @@ void VendingMachine::purchaseItem() {
   }
 }
 
+// save all changed to the data files
 void VendingMachine::saveData(string foodFilePath, string coinFilePath) {
   this->loader.writeCoinData(coinFilePath, this->bank);
   this->loader.writeFoodData(foodFilePath, this->foods);
 }
 
+// display menu
 void VendingMachine::displayItems() { this->foods->displayItems(); }
 
+// display information on coins, their number and total in bank
 void VendingMachine::displayBalance() { this->bank->displayBalance(); }
 
+// return all coins paid by the customer from the bank
 void VendingMachine::cancelPurchase(vector<int> paidBills) {
   for (auto &it : paidBills) {
     bank->manageBalance(it, SUBTRACT, 1);
   }
 }
 
+// take back all the coins given for refund back to bank
 void VendingMachine::cancelRefund(vector<int> refundedBills) {
   for (auto &it : refundedBills) {
     bank->manageBalance(it, ADD, 1);
   }
 }
 
+// return the spare changes after the purchase
 bool VendingMachine::refund(unsigned int amount, int index,
                             string &changeOutput, vector<int> refundedBills) {
   bool possible = false;
 
+  // anchor for the recursion function
   if (amount == 0) {
     possible = true;
   } else if (index < NUM_DENOMS) {
@@ -285,13 +308,14 @@ bool VendingMachine::refund(unsigned int amount, int index,
     for (int i = Range; i >= 0 && !possible; i--) {
       int Remainingamount =
           amount - (i * this->bank->getCoin(index)->getDenom());
-      // recursion function start here
+      // recursion
       possible =
           refund(Remainingamount, index + 1, changeOutput, refundedBills);
       if (possible) {
         // necessary when there are more than 1 coin of the same denomination to refund
         int return_count = i;
         while (return_count > 0) {
+          // write the change in required format
           if (this->bank->getCoin(index)->getDenom() < 100) {
             int coin = this->bank->getCoin(index)->getDenom();
             changeOutput += std::to_string(coin) + "c ";
@@ -300,6 +324,7 @@ bool VendingMachine::refund(unsigned int amount, int index,
             changeOutput += "$" + std::to_string(dollar / 100);
             changeOutput += " ";
           }
+          // remove a coin of this denomination from the bank
           bank->getCoin(index)->addCount(-1);
           return_count--;
         }
